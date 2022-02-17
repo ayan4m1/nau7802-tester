3beb9fdd1a6a2dd8bc0e73c2dafbddbb8a5f45b1
ayan4m1 <andrew@bulletlogic.com>
2022-02-17 14:53:36 -0500

    overhaul code to use three-point interpolated calibration

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
-#include <Adafruit_NAU7802.h>
 #include <Arduino.h>
-#include <Smoothed.h>
+#include <ESP_EEPROM.h>
+#include <SparkFun_Qwiic_Scale_NAU7802_Arduino_Library.h>
 
-Adafruit_NAU7802 loadCell;
+struct Settings {
+  int32 readings[3];
+  float factors[3];
+} settings;
+
+NAU7802 loadCell;
 static bool initialized = false;
-static uint32_t offset = 0;
-static float divider = 1;
 
 void calibrate() {
-  int32_t newOffset = 0;
   Serial.println(F("Send 't' to tare and continue."));
 
   boolean _resume = false;
@@ -16,11 +18,57 @@ void calibrate() {
     if (Serial.available() > 0) {
       char inByte = Serial.read();
       if (inByte == 't') {
-        newOffset = loadCell.read();
+        loadCell.calculateZeroOffset();
         _resume = true;
       }
     }
   }
+
+  Serial.println(F("Place a dime on load cell and send 'c'."));
+
+  _resume = false;
+  while (_resume == false) {
+    if (Serial.available() > 0) {
+      char inByte = Serial.read();
+      if (inByte == 'c') {
+        _resume = true;
+      }
+    }
+  }
+
+  float dimeCal = 0;
+  for (uint8_t i = 0; i < 5; i++) {
+    loadCell.calculateCalibrationFactor(2.268);
+    dimeCal += loadCell.getCalibrationFactor();
+  }
+  settings.readings[0] = loadCell.getAverage(8);
+  settings.factors[0] = dimeCal / 5.0;
+
+  Serial.printf("Raw 2.268g: %d\n", settings.readings[0]);
+  Serial.printf("Cal 2.268g: %.4f\n", settings.factors[0]);
+
+  Serial.println(F("Place a nickel on load cell and send 'c'."));
+
+  _resume = false;
+  while (_resume == false) {
+    if (Serial.available() > 0) {
+      char inByte = Serial.read();
+      if (inByte == 'c') {
+        _resume = true;
+      }
+    }
+  }
+
+  float nickelCal = 0;
+  for (uint8_t i = 0; i < 5; i++) {
+    loadCell.calculateCalibrationFactor(5.0);
+    nickelCal += loadCell.getCalibrationFactor();
+  }
+  settings.readings[1] = loadCell.getAverage(8);
+  settings.factors[1] = nickelCal / 5.0;
+
+  Serial.printf("Raw 5g: %d\n", settings.readings[1]);
+  Serial.printf("Cal 5g: %.4f\n", settings.factors[1]);
 
   Serial.println(F("Place 10g on load cell and send 'c'."));
 
@@ -34,43 +82,32 @@ void calibrate() {
     }
   }
 
-  Smoothed<float> tenGrams;
-  tenGrams.begin(SMOOTHED_AVERAGE, 10);
-  for (uint8_t i = 0; i < 10; i++) {
-    tenGrams.add(loadCell.read() / 10.0);
+  float tenGramCal = 0;
+  for (uint8_t i = 0; i < 5; i++) {
+    loadCell.calculateCalibrationFactor(10.0);
+    tenGramCal += loadCell.getCalibrationFactor();
   }
+  settings.readings[2] = loadCell.getAverage(8);
+  settings.factors[2] = tenGramCal / 5.0;
 
-  Serial.println(F("Place 200g on load cell and send 'c'."));
+  Serial.printf("Raw 10g: %d\n", settings.readings[2]);
+  Serial.printf("Cal 10g: %.4f\n", settings.factors[2]);
 
-  _resume = false;
-  while (_resume == false) {
-    if (Serial.available() > 0) {
-      char inByte = Serial.read();
-      if (inByte == 'c') {
-        _resume = true;
-      }
-    }
-  }
-
-  Smoothed<float> fullWeight;
-  fullWeight.begin(SMOOTHED_AVERAGE, 10);
-  for (uint8_t i = 0; i < 10; i++) {
-    fullWeight.add(loadCell.read() / 200.0);
-  }
-
-  float newDivider = (tenGrams.get() + fullWeight.get()) / 2.0;
-
-  Serial.printf("New divider is %f\n", newDivider);
-  Serial.printf("New tare offset is %d\n", newOffset);
-  Serial.print(F("Save this value? y/n"));
+  Serial.printf("(%d %.2f) (%d %.2f) (%d %.2f)", settings.readings[0],
+                settings.factors[0], settings.readings[1], settings.factors[1],
+                settings.readings[2], settings.factors[2]);
+  Serial.print(F("Save these values? y/n"));
 
   _resume = false;
   while (_resume == false) {
     if (Serial.available() > 0) {
       char inByte = Serial.read();
       if (inByte == 'y') {
-        divider = newDivider;
-        offset = newOffset;
+        EEPROM.put(0, settings);
+        if (!EEPROM.commit()) {
+          Serial.println(F("Failed to write to EEPROM!"));
+        }
+        // loadCell.setCalibrationFactor(calFactor);
         Serial.println(F("Calibration saved!"));
         _resume = true;
       } else if (inByte == 'n') {
@@ -84,6 +121,8 @@ void calibrate() {
 }
 
 void setup() {
+  EEPROM.begin(sizeof(Settings));
+
   Serial.begin(115200);
 
   while (!Serial) {
@@ -92,54 +131,129 @@ void setup() {
 
   Serial.println(F("Connected!"));
 
-  if (!loadCell.begin()) {
+  Wire.begin();
+  Wire.setClock(4000000);
+
+  // Init communication to sensor
+  if (!loadCell.begin(Wire, false)) {
     Serial.println(F("Failed to connect to NAU7802"));
     return;
   }
 
-  loadCell.setLDO(NAU7802_EXTERNAL);
-  loadCell.setGain(NAU7802_GAIN_1);
-  loadCell.setRate(NAU7802_RATE_10SPS);
-
-  for (uint8_t i = 0; i < 10; i++) {
-    while (!loadCell.available()) {
-      delayMicroseconds(100);
-    }
-
-    loadCell.read();
+  // Reset all registers
+  if (!loadCell.reset()) {
+    Serial.println(F("Failed to reset"));
+    return;
   }
 
-  while (!loadCell.calibrate(NAU7802_CALMOD_INTERNAL)) {
+  // Use external clock source
+  // if (!loadCell.setBit(NAU7802_PU_CTRL_OSCS, NAU7802_PU_CTRL)) {
+  //   Serial.println(F("Failed to set CTRL_OSCS"));
+  //   return;
+  // }
+
+  // Bypass LDO for direct AVDD supply
+  // if (!loadCell.clearBit(NAU7802_PU_CTRL_AVDDS, NAU7802_PU_CTRL)) {
+  //   Serial.println(F("Failed to bypass LDO"));
+  //   return;
+  // }
+
+  // Set LDO to highest possible voltage
+  if (!loadCell.setLDO(NAU7802_LDO_4V5)) {
+    Serial.println(F("Failed to set LDO"));
+    return;
+  }
+
+  // Power up sensor
+  if (!loadCell.powerUp()) {
+    Serial.println(F("Failed to power up"));
+    return;
+  }
+
+  // Set gain
+  if (!loadCell.setGain(NAU7802_GAIN_32)) {
+    Serial.println(F("Failed to set gain"));
+    return;
+  }
+
+  // Set sample rate
+  if (!loadCell.setSampleRate(NAU7802_SPS_10)) {
+    Serial.println(F("Failed to set sample rate"));
+    return;
+  }
+
+  // Disable clock chopper
+  if (!loadCell.setRegister(NAU7802_ADC, 0x30)) {
+    Serial.println(F("Failed to disable CLK_CHP"));
+    return;
+  }
+
+  // Enable 330pF decoupling cap on chan 2.
+  if (!loadCell.setBit(NAU7802_PGA_PWR_PGA_CAP_EN, NAU7802_PGA_PWR)) {
+    Serial.println(F("Failed to set up PWR_PGA_CAP_EN"));
+    return;
+  }
+
+  // Calibrate analog front end
+  while (!loadCell.calibrateAFE()) {
     Serial.println(F("Failed to calibrate system, retrying..."));
     delay(100);
   }
 
-  while (!loadCell.calibrate(NAU7802_CALMOD_OFFSET)) {
-    Serial.println(F("Failed to calibrate offset, retrying..."));
-    delay(100);
+  Serial.println(F("Setting zero offset..."));
+
+  // Set up scale parameters
+  loadCell.calculateZeroOffset();
+
+  EEPROM.get(0, settings);
+  if (settings.readings[0] != 0) {
+    // loadCell.setCalibrationFactor(calFactor);
+    Serial.println(F("Loaded calibration!"));
+    Serial.printf("(%d %.2f) (%d %.2f) (%d %.2f)", settings.readings[0],
+                  settings.factors[0], settings.readings[1],
+                  settings.factors[1], settings.readings[2],
+                  settings.factors[2]);
+  } else {
+    calibrate();
   }
 
   initialized = true;
+  Serial.println(F("Initialized!"));
 }
 
 void loop() {
+  // Bail if init failed
   if (!initialized) {
     Serial.println(F("Init failed, need to restart!"));
     delay(1000);
     return;
   }
 
+  // Check for calibration request
   if (Serial.available()) {
     char typedChar = Serial.read();
     if (typedChar == 'c') {
       calibrate();
+    } else if (typedChar == 't') {
+      Serial.println(F("Taring..."));
+      loadCell.calculateZeroOffset();
     }
   } else if (loadCell.available()) {
-    int32_t raw = loadCell.read();
-    Serial.printf("Raw value %d\n", raw);
-    float scaled = raw / divider;
-    Serial.printf("Scaled value %.2f\n", scaled);
-    float value = scaled + offset;
-    Serial.printf("Offset value %.2f\n", value);
+    float x0 = settings.readings[0];
+    float x1 = settings.readings[1];
+    float x2 = settings.readings[2];
+    float y0 = settings.factors[0];
+    float y1 = settings.factors[1];
+    float y2 = settings.factors[2];
+    float x = loadCell.getAverage(8);
+    float calFactor = ((((x - x1) * (x - x2)) / ((x0 - x1) * (x0 - x2))) * y0) +
+                      ((((x - x0) * (x - x2)) / ((x1 - x0) * (x1 - x2))) * y1) +
+                      ((((x - x0) * (x - x1)) / ((x2 - x0) * (x2 - x1))) * y2);
+
+    Serial.printf("Cal factor %.2f\n", calFactor);
+    float mass = (x - loadCell.getZeroOffset()) / calFactor;
+
+    Serial.printf("Raw value %d\n", x);
+    Serial.printf("Scaled value %.2f\n", mass);
   }
 }
